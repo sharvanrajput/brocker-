23846b8f0e5dae49e57ce2e14f121736c409128e
sharvanrajput <sharvanrajput098@gmail.com>
2025-04-14 15:35:53 +0530

    account type page done

---
+++ account-type.php
@@ -0,0 +1,111 @@
+<?php include('./includes/header.php') ?>
+<main>
+
+
+    <!--====== Start Page-banner section ======-->
+    <section class="page-banner bg_cover position-relative z-1">
+        <div class="shape shape-one scene"><span data-depth="1"><img src="assets/images/shape/shape-1.png"
+                    alt=""></span></div>
+        <div class="shape shape-two scene"><span data-depth="2"><img src="assets/images/shape/shape-2.png"
+                    alt=""></span></div>
+        <div class="shape shape-three scene"><span data-depth="3"><img src="assets/images/shape/shape-3.png"
+                    alt=""></span></div>
+        <div class="shape shape-four scene"><span data-depth="4"><img src="assets/images/shape/shape-2.png"
+                    alt=""></span></div>
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-6">
+                    <div class="page-title">
+                        <h1>Account Type</h1>
+                        <ul class="breadcrumbs-link">
+                            <li><a href="index.html">Home</a></li>
+                            <li class="active">Account type</li>
+                        </ul>
+                    </div>
+                </div>
+                <div class="col-lg-6">
+                    <div class="page-banner-img">
+                        <img src="assets/images/breadcrumb/img-2.jpg" alt="">
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section><!--====== End Page-banner section ======-->
+   
+     <!--====== Start pricing section ======-->
+ <section class="pricing-area pricing-area-v1  pattern-bg pt-130 pb-120">
+        <div class="shape shape-one scene"><span data-depth="1"><img src="assets/images/shape/shape-9.png"
+                    alt=""></span></div>
+        <div class="container">
+           
+            <div class="row justify-content-center">
+                <div class="col-lg-4 col-md-6 col-sm-12">
+                    <div class="pricing-item pricing-item-two bg-white mb-40 wow fadeInUp" data-wow-delay=".2s">
+                        <div class="pricing-head text-center">
+                            <span class="plan">Basic Plan</span>
+                            <h2 class="price"><span class="currency">$</span>16.95</h2>
+                        </div>
+                        <div class="pricing-body">
+                            <p>Sit amet consectetur adiising seddo eiusmod teme</p>
+                            <ul class="pricing-list">
+                                <li class="check">Web Design</li>
+                                <li class="check">Web Development</li>
+                                <li class="check">SEO Optimizations</li>
+                                <li class="uncheck">Online Support</li>
+                                <li class="uncheck">Domain & Hosting</li>
+                            </ul>
+                            <a href="service-details.html" class="main-btn arrow-btn bordered-btn btn-blue">Choose
+                                Plan</a>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-4 col-md-6 col-sm-12">
+                    <div class="pricing-item pricing-item-two bg-white mb-40 wow fadeInUp" data-wow-delay=".3s">
+                        <div class="ribbon">Popular</div>
+                        <div class="pricing-head text-center">
+                            <span class="plan">Standard Plan</span>
+                            <h2 class="price"><span class="currency">$</span>46.95</h2>
+                        </div>
+                        <div class="pricing-body">
+                            <p>Sit amet consectetur adiising seddo eiusmod teme</p>
+                            <ul class="pricing-list">
+                                <li class="check">Web Design</li>
+                                <li class="check">Web Development</li>
+                                <li class="check">SEO Optimizations</li>
+                                <li class="check">Online Support</li>
+                                <li class="check">Domain & Hosting</li>
+                            </ul>
+                            <a href="service-details.html" class="main-btn arrow-btn bordered-btn btn-blue">Choose
+                                Plan</a>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-4 col-md-6 col-sm-12">
+                    <div class="pricing-item pricing-item-two bg-white mb-40 wow fadeInUp" data-wow-delay=".4s">
+                        <div class="pricing-head text-center">
+                            <span class="plan">Silver Plan</span>
+                            <h2 class="price"><span class="currency">$</span>96.95</h2>
+                        </div>
+                        <div class="pricing-body">
+                            <p>Sit amet consectetur adiising seddo eiusmod teme</p>
+                            <ul class="pricing-list">
+                                <li class="check">Web Design</li>
+                                <li class="check">Web Development</li>
+                                <li class="check">SEO Optimizations</li>
+                                <li class="check">Online Support</li>
+                                <li class="check">Domain & Hosting</li>
+                            </ul>
+                            <a href="service-details.html" class="main-btn arrow-btn bordered-btn btn-blue">Choose
+                                Plan</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section><!--====== End pricing section ======-->
+ 
+
+
+
+</main>
+<?php include('./includes/footer.php') ?>
